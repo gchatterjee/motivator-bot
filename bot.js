07e619c48054f3b5994f0ce58b7e438f7f57c7c9
Gaurav Chatterjee <gchatter@andrew.cmu.edu>
2017-11-13 04:22:44 -0500

    ping

---
+++ bot.js
@@ -0,0 +1,33 @@
+var Discord = require('discord.io');
+var auth = require('./auth.json');
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    console.log('Connected')
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
