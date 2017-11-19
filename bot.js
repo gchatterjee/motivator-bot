e9ae9b3c00cbb0af8d768f6bc6625799420ee2a7
Gaurav Chatterjee <gchatter@andrew.cmu.edu>
2017-11-18 22:02:40 -0500

    added basic response to despair phrases and game detection

--- bot.js
+++ bot.js
@@ -11,27 +11,81 @@ const token = auth.token
 // constants
 const jeansId = '157610741858304000';
 const botId = '379491763414368261';
+const jobsId = '364834807043063819';
+const gamesId = '';
+var jeans = null;
+
+var despairRegexes = [];
 
 // helper functions
 function isJeans(user) {
     return (user.id === jeansId);
 }
 
+function isBot(user) {
+    return (user.id === botId);
+}
+
+function loadDespairPhrases() {
+    const despairPhrases = require('./jeans-despair.json');
+    var despairRegexes = []
+    for(let phrase of despairPhrases.phrases) {
+        despairRegexes.push(new RegExp(phrase));
+    }
+    return despairRegexes;
+}
+
+function getChannels(guild, idList) {
+    let channels = [];
+    for(id of idList) {
+        let chan = guild.channels.get(id);
+        if(chan !== undefined && chan !== null) {
+            channels.push(chan);
+        }
+    }
+    return channels;
+}
+
 // main
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('Successfully connected.');
+  despairRegexes = loadDespairPhrases();
 });
 
 // Create an event listener for messages
 client.on('message', message => {
-    console.log(message.content);
+    // console.log(message.content);
     if ((message.content.toLowerCase() === 'fuck <@' + botId + '>') ||
         (message.content.toLowerCase() === 'fuck you <@' + botId + '>')) {
-    message.channel.send('Fuck you too!');
+        console.log(message.author.username + ': ' + message.content);
+        console.log('me: Fuck you too, <@' + message.author.id + '>!')
+        message.channel.send('Fuck you too, <@' + message.author.id + '>!');
     }
-    if (isJeans(message.author)) {
+    else if (isJeans(message.author)) {
+        if (jeans === null) { jeans = message.author; }
+        let despairFlag = false;
+        for(exp of despairRegexes) {
+            if(exp.test(message.content.toLowerCase())) { despairFlag = true; }
+        }
+        if (despairFlag) {
+            console.log(message.author.username + ': ' + message.content);
+            console.log('me: Hi <@' + jeansId + '>. I sense that you are in despair.')
+            message.channel.send('Hi <@' + jeansId + '>. I sense that you are in despair.')
+        } else {
+
+        }
     }
 });
+
+client.on('presenceUpdate', update => {
+    if(isJeans(update.newMember) && jeans.presence.game !== null) {
+        var channels = getChannels(update.newMember.guild, [jobsId, gamesId]);
+        console.log('me: Stop playing ' + jeans.presence.game.name + ', <@' + jeansId + '>! Apply to some <#' + jobsId + '>!');
+        for (channel of channels) {
+            channel.send('Stop playing ' + jeans.presence.game.name + ', <@' + jeansId + '>! Apply to some <#' + jobsId + '>!');
+        }
+    }
+})
 
 // Log our bot in
 client.login(token);
